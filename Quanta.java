1c2fc8e54bfbdfdc62364047b31508e8b65637f3
GibranMo <Gibran@Gibrans-MacBook-Pro-677.local>
2015-02-08 18:16:47 -0800

    Deleted comments

--- Quanta.java
+++ Quanta.java
@@ -5,15 +5,6 @@ import java.util.Random;
 
 public class Quanta {
 	
-	/*We decided, 1 quanta = 1000 millisecond = 1 second
-	 * This way, if we generate a process with a random value of 0.1 quanta,
-	 * it will not be too small for the scheduling and timers of the computer.
-	 * With this set value, the smallest possible quanta is 100 milliseconds or .1 seconds,
-	 * which is manageable. 
-	*/
-	//private float timeSlice = 1000; //milliseconds
-	//private float testTimeSlice = 5;
-	
 	private Process quantasProcess;
 	
 	public Quanta(Process p) {
