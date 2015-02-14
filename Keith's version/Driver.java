6ebfebd6ed7a220cc8e269410908a6c3bb21f074
GibranMo <Gibran@Gibrans-MacBook-Pro-677.local>
2015-02-13 18:49:30 -0800

    Tried to restructure based on Keith's ideas
    I moved sorting of the process to the QuantaTimeLine class

--- Keith's version/Driver.java
+++ Keith's version/Driver.java
@@ -3,48 +3,37 @@ import java.util.*;
 
 
 public class Driver {
-	//Number of run simulations
+    //Number of run simulations
     private final static int NUM_OF_PROCESSES = 60;
-    static ArrayList <Process> setOfProcesses = new ArrayList <Process> (NUM_OF_PROCESSES);
-    static ArrayList <Process> readyQueue = new ArrayList <Process> ();
+    static ArrayList <Process> readyQueue = new ArrayList <Process> (NUM_OF_PROCESSES);
+    //static ArrayList <Process> readyQueue = new ArrayList <Process> ();
     
     public static void main (String args []) {
         
         populateProcesses();
-        printStuff(setOfProcesses);
         
-        //sorts setOfProcesses
-        sortProcesses();
-        readyQueue = setOfProcesses;
-        printStuff(setOfProcesses);
         
-        //Use QuantaTimeline as a simulator???
-        QuantaTimeLine qt = new QuantaTimeLine();
-        qt.FCFS(readyQueue);
-        printResults(qt.getFCFSResults());
+        printStuff(readyQueue);
         
         
         //Example of simulated run and results
         QuantaTimeLine run1 = new QuantaTimeLine();
         run1.FCFS(readyQueue);
-        run1.SJF(readyQueue);
-        run1.SRT(readyQueue);
-        run1.RR(readyQueue);
-        run1.HPFNonPre(readyQueue);
-        run1.HPFPre(readyQueue);
         printResults(run1.getFCFSResults());
+        /* Not implemented yet
+         run1.SJF(readyQueue);
+         run1.SRT(readyQueue);
+         run1.RR(readyQueue);
+         run1.HPFNonPre(readyQueue);
+         run1.HPFPre(readyQueue);
+         */
+        run1.SJF(readyQueue);
         printResults(run1.getSJFResults());
-        printResults(run1.getSRTResults());
-        printResults(run1.getRRResults());
-    }
-    
-    public static void sortProcesses() {
-        //Implement Comparator Interface so I can sort Processes by Arrival Time
-        Collections.sort(setOfProcesses, new Comparator<Process> () {
-            @Override public int compare(Process p1, Process p2) {
-                return Float.compare(p1.getArrivalTime(), p2.getArrivalTime());
-            }
-        });
+        /*
+         printResults(run1.getSJFResults());
+         printResults(run1.getSRTResults());
+         printResults(run1.getRRResults());
+         */
     }
     
     public static void populateProcesses(){
@@ -52,7 +41,7 @@ public class Driver {
         while (i < NUM_OF_PROCESSES )
         {
             Process newProcess = new Process("P" + String.format("%02d", i));
-            setOfProcesses.add(newProcess);
+            readyQueue.add(newProcess);
             i++;
         }
     }
@@ -72,7 +61,6 @@ public class Driver {
             int priority = processes.get(i).getPriority();
             String name = processes.get(i).getProcessName();
             sumExpRunTime += runTime;
-            //System.out.println(name + "\t\t" + arrival + "\t\t" + runTime + "\t\t" + priority);
             System.out.format("%s\t%08.5f\t%08.5f\t%d\n", name, arrival, runTime, priority);
             i++;
             
@@ -82,17 +70,17 @@ public class Driver {
     }
     
     //Print function to output simulation results and statistics
-    public static void printResults(SimResults results){    	
-    	//print timeline
-    	System.out.println("\nTime line: ");
-    	results.printTimeline();
-    	System.out.println("\n");
-    	
-    	//print calculated stats
-    	System.out.println("Average turnaround time:\t" + results.getAverageTurnaround());
-    	System.out.println("Average waiting time:\t\t" + results.getAverageWaiting());
-    	System.out.println("Average response time:\t\t" + results.getAverageResponse());
-    	System.out.println("Throughput:\t\t\t" + results.getThroughput());
+    public static void printResults(SimResults results){
+        //print timeline
+        System.out.println("\nTime line: ");
+        results.printTimeline();
+        System.out.println("\n");
+        
+        //print calculated stats
+        System.out.println("Average turnaround time:\t" + results.getAverageTurnaround());
+        System.out.println("Average waiting time:\t\t" + results.getAverageWaiting());
+        System.out.println("Average response time:\t\t" + results.getAverageResponse());
+        System.out.println("Throughput:\t\t\t" + results.getThroughput());
     }
     
 }
--- Keith's version/QuantaTimeLine.java
+++ Keith's version/QuantaTimeLine.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
 //Represents the timeline of time chunks
 public class QuantaTimeLine {
-	
-	//number of quanta to reach, sim results may have 100+
+    
+    //number of quanta to reach, sim results may have 100+
     private final static int QUANTA_LIMIT = 100;
     
     //Store results for each algorithm run
@@ -15,9 +17,9 @@ public class QuantaTimeLine {
     private SimResults RRResults;
     private SimResults HPFPreResults;
     private SimResults HPFNonPreResults;
-	
+    
     private static ArrayList <Quanta >quantaLine = new ArrayList <Quanta>();
-    public float quantaSize = 3; // Equal to 3 seconds  (Hence, you will see the number 3000 representing milliseconds
+    public float quantaSize = 1;
     
     //Just allocate 100 spaces
     public QuantaTimeLine() {
@@ -31,18 +33,27 @@ public class QuantaTimeLine {
         }
     }
     
-    //TODO: Gibran
+    
     //First Come First Served.
     public void FCFS (ArrayList<Process> listOfProcesses){
-    	
-    	FCFSResults = new SimResults();
-    	int sumOfCompletedProcesses = 0;
-    	        
+        
+        FCFSsort(listOfProcesses);
+        
+        
+        FCFSResults = new SimResults();
+        int sumOfCompletedProcesses = 0;
+        ArrayList<Integer> setTurnAroundTimes = new ArrayList <Integer> ();
+        ArrayList<Integer> setWaitingTimes = new ArrayList <Integer> ();
+        
+        
+        
         int i = 0; //for indexing the ready process queue
         int j = 0; //for indexing the quanta time line. At the beginning, it will be synchronized with 'i' but then it will
         //start getting ahead.
         while (i < listOfProcesses.size()) {
             
+            setWaitingTimes.add(j);
+            //System.out.println("inserting " + j );
             if (j == quantaLine.size())
             {
                 //forget about the rest of the process left in the listOfProcess. Quanta Line is full at this point
@@ -52,14 +63,14 @@ public class QuantaTimeLine {
             quantaLine.get(j).useUpQuanta();//** Immediately burn next quanta spot. (The logic for deciding
             //********************************** whether or not more quanta slots or needed comes right up next).
             
-            //****OFFICIAL REQUIRED OUTPUT - Site 1 (there is another spot below  where this output is needed)(Matching quanta spot with process)
-            System.out.println(">>quanta spot-" + j + " process name: " + listOfProcesses.get(i).getProcessName() ); //Since we are doing FCFS, 'i' represents the name of each process
-            //*******************************************************************
+            
+            //System.out.println(">>quanta spot-" + j + " process name: " + listOfProcesses.get(i).getProcessName() ); //Since we are doing FCFS, 'i' represents the name of each process
+            
             
             //Add process to timeline
             FCFSResults.addToTimeline(listOfProcesses.get(i));
             
-           //increment num of completed processes
+            //increment num of completed processes
             sumOfCompletedProcesses++;
             
             //Check if the expected runtime of current process (i) will fit into 1 quanta which is 3.0
@@ -82,11 +93,15 @@ public class QuantaTimeLine {
                     }
                     //advanceQuantaPosition(j, listOfProcesses); //Use up quanta time line while remaining at the same remaining at same ready process index
                     quantaLine.get(j).useUpQuanta();
-                    System.out.println(">>quanta spot-" + j + " process name: " + listOfProcesses.get(i).getProcessName());
+                    //System.out.println(">>quanta spot-" + j + " process name: " + listOfProcesses.get(i).getProcessName());
                     
                     //add process to timeline
                     FCFSResults.addToTimeline(listOfProcesses.get(i));
+                    
+                    
                     j++;
+                    setTurnAroundTimes.add(j);
+                    //System.out.println("inserting " + j );
                     
                 }
                 
@@ -96,75 +111,205 @@ public class QuantaTimeLine {
             
             i++;
             j++;
+            setTurnAroundTimes.add(j);
+            //System.out.println("inserting " + j );
+            
             
         }
         
         //Set num of completed processes
         FCFSResults.setNumOfCompletedProcesses(sumOfCompletedProcesses);
         
+        int sum1 = 0;
+        for (int k = 0; k < setTurnAroundTimes.size(); k ++)
+            sum1 += setTurnAroundTimes.get(k);
+        
+        FCFSResults.setSumOfTurnaround(sum1);
+        
+        int sum2 = 0;
+        for (int k = 0; k < setWaitingTimes.size(); k++)
+            sum2 += setWaitingTimes.get(k);
+        
+        FCFSResults.setSumOfWaiting(sum2);
+        
     }
     
-    //TODO: Gibran
-    public void SJF (ArrayList<Process> processes){
-    	
+    
+    /*
+     * Exact same algorithm as FCFS, but 'listOfProcesses' is received here sorted by expected runtime.
+     */
+    public void SJF (ArrayList<Process> listOfProcesses){
+        
+        
+        SJFsort(listOfProcesses);
+        SJFResults = new SimResults();
+        int sumOfCompletedProcesses = 0;
+        ArrayList<Integer> setTurnAroundTimes = new ArrayList <Integer> ();
+        ArrayList<Integer> setWaitingTimes = new ArrayList <Integer> ();
+        
+        
+        
+        int i = 0; //for indexing the ready process queue
+        int j = 0; //for indexing the quanta time line. At the beginning, it will be synchronized with 'i' but then it will
+        //start getting ahead.
+        while (i < listOfProcesses.size()) {
+            
+            setWaitingTimes.add(j);
+            //System.out.println("inserting " + j );
+            if (j == quantaLine.size())
+            {
+                //forget about the rest of the process left in the listOfProcess. Quanta Line is full at this point
+                break;
+            }
+            //**********************************
+            quantaLine.get(j).useUpQuanta();//** Immediately burn next quanta spot. (The logic for deciding
+            //********************************** whether or not more quanta slots or needed comes right up next).
+            
+            
+            //System.out.println(">>quanta spot-" + j + " process name: " + listOfProcesses.get(i).getProcessName() ); //Since we are doing FCFS, 'i' represents the name of each process
+            
+            
+            //Add process to timeline
+            FCFSResults.addToTimeline(listOfProcesses.get(i));
+            
+            //increment num of completed processes
+            sumOfCompletedProcesses++;
+            
+            //Check if the expected runtime of current process (i) will fit into 1 quanta which is 3.0
+            //float processTimeRem = quantaLine.get(i).getProcessTimeRemaining(); //extract time of current process
+            float processTimeRem = listOfProcesses.get(i).getExpRunTime();
+            
+            if (processTimeRem > quantaSize)
+            {
+                //Ceiling Function - Rounding up Any decimal to a whole number
+                int additonalQuantas = (int) Math.ceil(processTimeRem);
+                
+                //use j to advance position in quanta time line
+                j++; //Process didn't fit in one quanta slice, so advance at least one spot in quanta time line
+                for (int k = 1; k < additonalQuantas; k++ ) //Iterate the number of times the process fits into quantas slices
+                {
+                    if (j >= quantaLine.size()) //need to expand quanta line
+                    {
+                        quantaLine.add(new Quanta());
+                        
+                    }
+                    //advanceQuantaPosition(j, listOfProcesses); //Use up quanta time line while remaining at the same remaining at same ready process index
+                    quantaLine.get(j).useUpQuanta();
+                    //System.out.println(">>quanta spot-" + j + " process name: " + listOfProcesses.get(i).getProcessName());
+                    
+                    //add process to timeline
+                    SJFResults.addToTimeline(listOfProcesses.get(i));
+                    
+                    
+                    j++;
+                    setTurnAroundTimes.add(j);
+                    //System.out.println("inserting " + j );
+                    
+                }
+                
+                i++;
+                continue;
+            }
+            
+            i++;
+            j++;
+            setTurnAroundTimes.add(j);
+            //System.out.println("inserting " + j );
+        }
+        
+        //Set num of completed processes
+        SJFResults.setNumOfCompletedProcesses(sumOfCompletedProcesses);
+        
+        int sum1 = 0;
+        for (int k = 0; k < setTurnAroundTimes.size(); k ++)
+            sum1 += setTurnAroundTimes.get(k);
+        
+        SJFResults.setSumOfTurnaround(sum1);
+        
+        int sum2 = 0;
+        for (int k = 0; k < setWaitingTimes.size(); k++)
+            sum2 += setWaitingTimes.get(k);
+        
+        SJFResults.setSumOfWaiting(sum2);
+        
+        
     }
     
     //TODO: Keith
     public SimResults SRT (ArrayList<Process> processes){
-    	SRTResults = new SimResults();
-    	double sumOfTurnaround = 0,sumOfWaiting = 0, sumOfResponse = 0;
-    	int sumOfCompletedProcesses = 0;
-    	/*
-    	 * Run simulation
-    	 * Set turnaround, waiting, response, throughput, timeline for results
-    	 */
-    	return SRTResults;
+        SRTResults = new SimResults();
+        double sumOfTurnaround = 0,sumOfWaiting = 0, sumOfResponse = 0;
+        int sumOfCompletedProcesses = 0;
+        /*
+         * Run simulation
+         * Set turnaround, waiting, response, throughput, timeline for results
+         */
+        return SRTResults;
     }
     
     //TODO: Keith
     public SimResults RR (ArrayList<Process> processes){
-    	RRResults = new SimResults();
-    	/*
-    	 * Run simulation
-    	 * Set turnaround, waiting, response, throughput, timeline for results
-    	 */
-    	return RRResults;
+        RRResults = new SimResults();
+        /*
+         * Run simulation
+         * Set turnaround, waiting, response, throughput, timeline for results
+         */
+        return RRResults;
     }
     
     //TODO: Graeme
     public void HPFPre (ArrayList<Process> processes){
-    	
+        
     }
     
     //TODO: Graeme
     public void HPFNonPre (ArrayList<Process> processes){
-    	
+        
     }
     
     /* RETURN SIMULATION RESULTS */
     public SimResults getFCFSResults(){
-    	return FCFSResults;
+        return FCFSResults;
     }
     
     public SimResults getSJFResults(){
-    	return SJFResults;
+        return SJFResults;
     }
     
     public SimResults getSRTResults(){
-    	return SRTResults;
+        return SRTResults;
     }
     
     public SimResults getRRResults(){
-    	return RRResults;
+        return RRResults;
     }
     
     public SimResults getHPFPreResults(){
-    	return HPFPreResults;
+        return HPFPreResults;
     }
     
     public SimResults getHPFNonPreResults(){
-    	return HPFNonPreResults;
+        return HPFNonPreResults;
     }
-
+    
+    private static void FCFSsort(ArrayList<Process> processes) {
+        //Implement Comparator Interface so I can sort Processes by Arrival Time
+        Collections.sort(processes, new Comparator<Process> () {
+            @Override public int compare(Process p1, Process p2) {
+                return Float.compare(p1.getArrivalTime(), p2.getArrivalTime());
+            }
+        });
+    }
+    
+    //Sorts the processes in ascending order of expected runtime.
+    public static void SJFsort(ArrayList<Process> processes) {
+        //Implement Comparator Interface so I can sort Processes by Arrival Time
+        Collections.sort(processes, new Comparator<Process> () { 
+            @Override public int compare(Process p1, Process p2) {
+                return Float.compare(p1.getExpRunTime(), p2.getExpRunTime());
+            }
+        });	
+    }
+    
     
 }
